4d874d5bbf89530b492e98a52d9fde6c6f901285
wxy531520 <155159629+wxy531520@users.noreply.github.com>
2024-09-01 09:51:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '王晨的作品集', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一名热爱设计的设计师', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://wuxuanyi531.cloudns.biz//', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://wangchen.us.kg/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '设计师, 作品', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
